9864e986b578547698bf4e0ac90b6ef8666add35
khoroshevDmV <51055754+khoroshevDmV@users.noreply.github.com>
2020-11-02 10:15:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,8 @@
-/*function count_of_people() {
-	switch(localStorage.length % 10 / 2) {
-		case 1:
-			count.innerHTML = "Нас уже " + (localStorage.length / 2) + " пользователь";
-			break;
-		case 2:
-		case 3:
-		case 4:
-			count.innerHTML = "Нас уже " + (localStorage.length / 2) + " пользователя";
-			break;
-		default:
-			count.innerHTML = "Нас уже " + (localStorage.length / 2) + " пользователей";
-	}
-}*/
 history_next.onclick = function() {
 	window.location = "/history.html";
 }
 memorable_places_next.onclick = function() {
 	window.location = "/memorable_places.html";
-}
-сulture_and_art_next.onclick = function() {
-	window.location = "/culture_and_art.html";
 }
 personality_next.onclick = function() {
 	window.location = "/personality.html";
